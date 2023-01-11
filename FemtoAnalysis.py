b013726ad616bec3b41be96f218d8791538004f6
Jaime <jaime.gonzalez@tum.de>
2023-01-11 16:02:33 +0100

    pp examination scripts

--- FemtoAnalysis.py
+++ FemtoAnalysis.py
@@ -1,1 +1,195 @@
-FemtoAnalysis.py
+import ROOT
+import correlation_handler as gentlefemto
+
+#the order of the names has to agree with the the order, in which the histogramms are saved
+#this is only a temporary solution and has to be done better!
+histnames = ["hSE", "hME", "hCF", "hPt", "hEta", "hPhi", "hDCAxy", "zvtxhist", "MultV0M", "MultNTracksPV"]
+
+def getCorrelationFunction(infilename, config, rebinfactor):
+    
+    """
+    Function to get the same event (se) and mixed event (me) distribution and calculate the 
+    correlation function using the correlation_handler class.
+
+    Parameters:
+
+    infilename = path and name to the AnalysisResults.root file, where the same event and mixed event distrubution is
+    
+    config = configuration name of the subwagon. Use an empty string ("") for the standard configuration.
+             use an underscore before the name of another configuration (e.g. "_ap-base") 
+
+    rebinfactor = rebinning factor to be applied ONLY to the correlation function within the correlation_handler class
+
+    Output:
+
+    array with the three histograms in the following order: se, me, cf 
+    """
+
+    infile = ROOT.TFile(infilename,"read")
+    directory = "femto-dream-pair-task-track-track"+config
+    
+    se = infile.Get(directory+"/SameEvent/relPairDist")
+    me = infile.Get(directory+"/MixedEvent/relPairDist")
+
+    se.SetDirectory(0) 
+    me.SetDirectory(0) 
+
+    infile.Close() 
+
+    ch = gentlefemto.CorrelationHandler("cf",se,me)
+    ch.rebin(rebinfactor)
+    ch.make_correlation_function()
+    ch.normalise(0.24,0.34)
+
+    
+    cf = ch.get_correlation_function()
+    histos = [se, me, cf]
+    return histos 
+
+def getSingleParticlePlots(infilename, config):
+    """
+    Function to get the histograms of the single particle properties, i.e.
+     - transverse momentum pT
+     - pseudorapidity eta
+     - angle phi
+     - transverse DCA 
+
+    Parameters:
+
+    infilename = path and name to the AnalysisResults.root file, where the same event and mixed event distrubution is
+    
+    config = configuration name of the subwagon. Use an empty string ("") for the standard configuration.
+             use an underscore before the name of another configuration (e.g. "_ap-base") 
+
+    Output:
+
+    array with the four histograms in the following order: hPt, hEta, hPhi, hDCAxy 
+    """
+    
+    directory = "femto-dream-pair-task-track-track"+config
+    
+    infile = ROOT.TFile(infilename, "read")
+    histos = []
+     
+    histos.append(infile.Get(directory+"/Tracks_one/hPt"))
+    histos.append(infile.Get(directory+"/Tracks_one/hEta"))
+    histos.append(infile.Get(directory+"/Tracks_one/hPhi"))
+    histos.append(infile.Get(directory+"/Tracks_one/hDCAxy"))
+    for hist in histos:
+        hist.SetDirectory(0)
+    
+    infile.Close()
+
+    return histos
+
+def getEventHistos(infilename, config):
+    """
+    Function to get the histograms of the event properties, i.e.
+     - position distribution of the primary vertex in z direction
+     - amplitude measured in the V0 detector (used as a multiplicity estimator)
+     - number of tracks contributing to finding the primary vertex (i.e. the multiplicity at the primary vertex)
+
+    Parameters:
+
+    infilename = path and name to the AnalysisResults.root file, where the same event and mixed event distrubution is
+    
+    config = configuration name of the subwagon. Use an empty string ("") for the standard configuration.
+             use an underscore before the name of another configuration (e.g. "_ap-base") 
+
+    Output:
+
+    array with the three histograms in the following order: zvtxhist, MultV0M, MultNTracksPV 
+    """
+    
+    directory = "femto-dream-pair-task-track-track"+config
+    
+    infile = ROOT.TFile(infilename, "read")
+    histos = []
+     
+    histos.append(infile.Get(directory+"/Event/zvtxhist"))
+    histos.append(infile.Get(directory+"/Event/MultV0M"))
+    histos.append(infile.Get(directory+"/Event/MultNTracksPV"))
+    for hist in histos:
+        hist.SetDirectory(0)
+    
+    infile.Close()
+
+    return histos
+
+
+def saveHistogramms(path, filename, config, rebin, newfile):
+    """
+    Function to save the histogramms of an "AnalysisResults.root" file to a new file.
+    Its name is the name of the infput file with the prefix "GF-output_".
+    It saves the output of the specified subwagon to an individual directory withing the file.
+    If you want the output of more substrains within the same output file, you have to call the function
+    multiple times and ster the parameter "newfile" to true only for the first time
+    
+    Parameters:
+    
+    path = path to the AnalysisResults.root file and the path, where the new root file will be stored
+    
+    filename = filename of the AnalysisResults.root file
+    
+    config = configuration name of the subwagon. Use an empty string ("") for the standard configuration.
+             use an underscore before the name of another configuration (e.g. "_ap-base") 
+    
+    rebin = rebinning factor to be applied ONLY to the correlation function within the correlation_handler class
+
+    newfile = bool value to create a new file or not.
+              -> for true the outputfile is called with the option "recreate"
+              -> for false the outputfile is created with the option "update"
+
+    Output:
+    
+    no output.. just saving the new root file
+    """
+    
+    infile = path+filename 
+
+    #Get the histogramms from the input file 
+    correlationHistos = getCorrelationFunction(infile, config, rebin)
+    trackHistos = getSingleParticlePlots(infile, config)
+    eventHistos = getEventHistos(infile, config)
+
+    histos = []
+    histos.extend(correlationHistos)
+    histos.extend(trackHistos)
+    histos.extend(eventHistos)
+    nTPC = histos[3].Integral(0,histos[3].FindBin(0.75))
+    nEvents = histos[7].GetEntries()
+
+
+    #print here some interesting numbers like number of pairs in a certain k^* region, number of events, etc. 
+    print(" ") 
+    print("### Getting file "+filename+" with the configuration "+config+" ###") 
+    
+    print("Number of Events:")
+    print(histos[0].GetEntries())
+    
+    print("nTPC/nEvents")
+    print(nTPC/nEvents)
+    
+    print("Number of pairs in the low k* region") 
+    print(histos[0].Integral(1,histos[0].FindBin(0.2)))
+    
+
+
+    if newfile:
+        fileaction = "recreate"
+    else:
+        fileaction = "update"
+
+    outfile = path+"GF-output_"+filename
+    output = ROOT.TFile(outfile,fileaction)
+    if config=="":
+        dir = output.mkdir("_std")
+    else:
+        dir = output.mkdir(config)
+    
+    dir.cd() 
+
+    for i in range(len(histos)):
+        histos[i].Write(histnames[i])
+
+    output.Close()
--- correlation_handler.py
+++ correlation_handler.py
@@ -1,1 +1,149 @@
-correlation_handler.py
+"""
+Module containing the class used to compute the correlation function
+from same-event and mixed-event distributions.
+"""
+
+from ROOT import TH1F
+import logging
+import numpy as np
+
+
+class CorrelationHandler:
+    """
+    Class used to compute the correlation function
+    from same-event and mixed-event k* distributions.
+    Performs rebinning, normalisation and advanced operations.
+
+    Parameters:
+    ____________________________________________________
+
+    same_event = same-event k* distribution (TH1F)
+
+    mixed_event = mixed-event k* distribution (TH1F)
+
+    """
+
+    def __init__(self, name, same_event=None, mixed_event=None) -> None:
+        self.name = name
+        if same_event:
+            self.set_same_event(same_event)
+        else:
+            self.same_event = None
+        if mixed_event:
+            self.set_mixed_event(mixed_event)
+        else:
+            self.mixed_event = None
+        self.correlation_function = None
+
+    def self_normalise(self) -> None:
+        if self.same_event:
+            n_same = self.same_event.GetNbinsX()
+            integral_same = self.same_event.Integral(1, n_same+1)
+            self.same_event.Scale(1/integral_same)
+        else:
+            logging.info('Same-event not set.')
+        if self.mixed_event:
+            n_mixed = self.mixed_event.GetNbinsX()
+            integral_mixed = self.mixed_event.Integral(1, n_mixed+1)
+            self.mixed_event.Scale(1/integral_mixed)
+        else:
+            logging.info('Mixed-event not set.')
+
+    def set_same_event(self, same_event=None) -> None:
+        self.same_event = same_event.Clone(f'hSame_{self.name}')
+
+    def set_mixed_event(self, mixed_event=None) -> None:
+        self.mixed_event = mixed_event.Clone(f'hMixed_{self.name}')
+
+    def move_to_MeV(self):
+        if self.same_event is None or self.mixed_event is None:
+            logging.error(
+                'Set same-event end/or mixed-event distribution first')
+        else:
+            if self.correlation_function:
+                logging.info(
+                    'After the change of units evaluate the CF again.')
+            nbins = self.same_event.GetNbinsX()
+            low_edge = self.same_event.GetBinLowEdge(1)
+            up_edge = self.same_event.GetBinLowEdge(nbins+1)
+            self.same_event.SetName('hSame_old')
+            self.mixed_event.SetName('hMixed_old')
+            hSame_new = TH1F(
+                f'hSame_{self.name}', r';k* (MeV/#it{c}); Entries', nbins, low_edge*1000, up_edge*1000)
+            hMixed_new = TH1F(
+                f'hMixed_{self.name}', r';k* (MeV/#it{c}); Entries', nbins, low_edge*1000, up_edge*1000)
+            for i in range(0, nbins+2):
+                hSame_new.SetBinContent(i, self.same_event.GetBinContent(i))
+                hSame_new.SetBinError(i, self.same_event.GetBinError(i))
+                hMixed_new.SetBinContent(i, self.mixed_event.GetBinContent(i))
+                hMixed_new.SetBinError(i, self.mixed_event.GetBinError(i))
+            self.same_event = hSame_new
+            self.mixed_event = hMixed_new
+
+    def move_to_GeV(self):
+        if self.same_event is None or self.mixed_event is None:
+            logging.error(
+                'Set same-event end/or mixed-event distribution first')
+        else:
+            if self.correlation_function:
+                logging.info(
+                    'After the change of units evaluate the CF again.')
+            nbins = self.same_event.GetNbinsX()
+            low_edge = self.same_event.GetBinLowEdge(1)
+            up_edge = self.same_event.GetBinLowEdge(nbins+1)
+            self.same_event.SetName('hSame_old')
+            self.same_event.SetName('hMixed_old')
+            hSame_new = TH1F(
+                f'hSame_{self.name}', r';k* (GeV/#it{c}); Entries', nbins, low_edge/1000, up_edge/1000)
+            hMixed_new = TH1F(
+                f'hMixed_{self.name}', r';k* (GeV/#it{c}); Entries', nbins, low_edge/1000, up_edge/1000)
+            for i in range(0, nbins+2):
+                hSame_new.SetBinContent(i, self.same_event.GetBinContent(i))
+                hSame_new.SetBinError(i, self.same_event.GetBinError(i))
+                hMixed_new.SetBinContent(i, self.mixed_event.GetBinContent(i))
+                hMixed_new.SetBinError(i, self.mixed_event.GetBinError(i))
+            self.same_event = hSame_new
+            self.mixed_event = hMixed_new
+
+    def rebin(self, n_rebin=2) -> None:
+        self.same_event.Rebin(n_rebin)
+        self.mixed_event.Rebin(n_rebin)
+
+    def make_correlation_function(self):
+        self.correlation_function = self.same_event.Clone(f'hCF_{self.name}')
+        self.correlation_function.Reset()
+        self.correlation_function.GetYaxis().SetTitle('C(k*)')
+        for i in range(1, self.correlation_function.GetNbinsX()):
+            vSame = self.same_event.GetBinContent(i)
+            eSame = self.same_event.GetBinError(i)
+            vMixed = self.mixed_event.GetBinContent(i)
+            eMixed = self.mixed_event.GetBinError(i)
+            if vSame < 1.e-12 or vMixed < 1.e-12:
+                continue
+            else:
+                vCF = vSame/vMixed
+                eCF = np.sqrt((eMixed/vMixed)*(eMixed/vMixed) +
+                              (eSame/vSame)*(eSame/vSame)) * vCF
+                self.correlation_function.SetBinContent(i, vCF)
+                self.correlation_function.SetBinError(i, eCF)
+
+    def get_correlation_function(self):
+        return self.correlation_function
+
+    def get_same_event(self):
+        return self.same_event
+
+    def get_mixed_event(self):
+        return self.mixed_event
+
+    def normalise(self, low=0.6, high=1.) -> None:
+        if self.correlation_function:
+            low_bin = self.correlation_function.FindBin(low)
+            low_edge = self.correlation_function.GetBinLowEdge(low_bin)
+            high_bin = self.correlation_function.FindBin(high)
+            high_edge = self.correlation_function.GetBinLowEdge(high_bin+1)
+            cf_integral = self.correlation_function.Integral(
+                low_bin, high_bin, 'width')
+            self.correlation_function.Scale((high_edge-low_edge)/cf_integral)
+        else:
+            logging.error('Compute the correlation function first')
