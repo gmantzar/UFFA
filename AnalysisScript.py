09686fcddcebae021f1130574061dfa300cb05fd
Jaime <jaime.gonzalez@tum.de>
2023-01-24 15:36:33 +0100

    removed duplicate functions after merging into one

--- AnalysisScript.py
+++ AnalysisScript.py
@@ -1,4 +1,4 @@
-import FemtoAnalysis
+import FemtoAnalysis as FemtoAnalysis
 
 
 # Here, I use the saveHistogramms function for every configuration and file I want to analyse
@@ -6,68 +6,13 @@ import FemtoAnalysis
 # for the variations within the same dataset in order to save the output in the same file
 
 
-########## LHC22m_pass1_subset ##########
-
-#LHC22m_pass1_subset_offset
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22m_pass1_subset/offset/"
-thefilename = "AnalysisResults_22m_pass1_subset_offset.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 2, True)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-base", 2, False)
-
-#LHC22m_pass1_subset_no-offset
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22m_pass1_subset/no-offset/"
-thefilename = "AnalysisResults_22m_pass1_subset_no-offset.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 2, True)
-
-########## LHC22m_pass2_subset ##########
-
-#LHC22m_pass1_subset_offset
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22m_pass2_subset/"
-thefilename = "AnalysisResults_22m_pass2_subset.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 2, True)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-base", 2, False)
-
-########## LHC22r_pass1_subset ##########
-
-#LHC22r_pass1_subset_offset
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22r_pass1_subset/offset/"
-thefilename = "AnalysisResults_22r_pass1_subset_offset.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 5, True)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-base", 5, False)
-
-#LHC22r_pass1_subset_no-offset
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22r_pass1_subset/no-offset/"
-thefilename = "AnalysisResults_22r_pass1_subset_no-offset.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 5, True)
-
-
-########## LHC22p_pass1 ##########
-
-#LHC22p_pass1_offset
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22p_pass1/offset/"
-thefilename = "AnalysisResults_22p_pass1_offset.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 2, True)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-base", 2, False)
-
-#LHC22p_pass1_no-offset
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22p_pass1/no-offset/"
-thefilename = "AnalysisResults_22p_pass1_no-offset.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 2, True)
-
-#LHC22f_pass2
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC22f_pass2/"
-thefilename = "AnalysisResults_22f_pass2.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 5, True)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_dca35", 5, False)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-base", 5, False)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-dca35", 5, False)
-
 ########## LHC18l ##########
-thepath = "~/femto/pprun3/TrainOutput/run20221230/LHC18l/"
-thefilename = "AnalysisResults_18l.root"
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "", 2, True)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_clusters100", 2, False)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_eta07", 2, False)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-base", 2, False)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-clusters100", 2, False)
-FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-eta07", 2, False)
+thepath = "~/work/projects/pp_python/"
+#thefilename = "AnalysisResults_18l.root"
+thefilename = "AnalysisResults_22m_pass2_no-offset.root"
+FemtoAnalysis.saveHistogramms(thepath, thefilename, True, "", 'th2')
+FemtoAnalysis.saveHistogramms(thepath, thefilename, False, "_ap-base", 'mult', [2, 4])
+#FemtoAnalysis.saveHistogramms(thepath, thefilename, "_clusters100", 2, False)
+#FemtoAnalysis.saveHistogramms(thepath, thefilename, "_eta07", 2, False)
+#FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-clusters100", 2, False)
+#FemtoAnalysis.saveHistogramms(thepath, thefilename, "_ap-eta07", 2, False)
--- FemtoAnalysis.py
+++ FemtoAnalysis.py
@@ -2,7 +2,7 @@ import ROOT
 import correlation_handler as gentlefemto
 
 
-def getCorrelationFunction(infilename, config, rebinfactor):
+def getCorrelationFunction(infilename, config, hist_type, rebinfactor):
 
     """
     Function to get the same event (se) and mixed event (me) distribution and calculate the
@@ -21,55 +21,22 @@ def getCorrelationFunction(infilename, config, rebinfactor):
 
     array with the three histograms in the following order: se, me, cf
     """
-
     infile = ROOT.TFile(infilename,"read")
-    directory = "femto-dream-pair-task-track-track"+config
+    directory = infile.GetDirectory("femto-dream-pair-task-track-track"+config)
+    if not directory:
+        print("TDirectory femto-dream-pair-task-track-track" + config + " does not exists!")
+        exit()
 
-    se = infile.Get(directory+"/SameEvent/relPairDist")
-    me = infile.Get(directory+"/MixedEvent/relPairDist")
+    if isinstance(rebinfactor, int):
+        rebinfactor = [rebinfactor]
 
-    se.SetDirectory(0)
-    me.SetDirectory(0)
-
-    infile.Close()
-
-    ch = gentlefemto.CorrelationHandler("cf",se,me)
-    ch.rebin(rebinfactor)
-    ch.make_correlation_function()
-    ch.normalise(0.24,0.34)
-
-    histos = [
-            ["hSE", ch.get_se_k()],
-            ["hME", ch.get_me_k()],
-            ["hCF", ch.get_correlation_function()],
-    ]
-    return histos
-
-def getCorrelationFunctionTH2(infilename, config, rebinfactor):
-
-    """
-    Function to get the same event (se) and mixed event (me) distribution from a k* vs mult (TH2)
-    and calculate the correlation function using the correlation_handler class.
-
-    Parameters:
-
-    infilename = path and name to the AnalysisResults.root file, where the same event and mixed event distrubution is
-
-    config = configuration name of the subwagon. Use an empty string ("") for the standard configuration.
-             use an underscore before the name of another configuration (e.g. "_ap-base")
-
-    rebinfactor = rebinning factor to be applied ONLY to the correlation function within the correlation_handler class
-
-    Output:
-
-    array with the three histograms in the following order: se, me, cf
-    """
-
-    infile = ROOT.TFile(infilename,"read")
-    directory = "femto-dream-pair-task-track-track"+config
-
-    se = infile.Get(directory+"/SameEvent/relPairkstarMult")
-    me = infile.Get(directory+"/MixedEvent/relPairkstarMult")
+    th2 = ['TH2', 'th2', 'mult', 'kmult']
+    if hist_type in th2:
+        se = directory.Get("SameEvent/relPairkstarMult")
+        me = directory.Get("MixedEvent/relPairkstarMult")
+    else:
+        se = directory.Get("SameEvent/relPairDist")
+        me = directory.Get("MixedEvent/relPairDist")
 
     se.SetDirectory(0)
     me.SetDirectory(0)
@@ -77,104 +44,34 @@ def getCorrelationFunctionTH2(infilename, config, rebinfactor):
     infile.Close()
 
     ch = gentlefemto.CorrelationHandler("cf", se, me)
-    ch.reweight()
-    ch.rebin(rebinfactor)
-    ch.make_correlation_function()
-    ch.make_cf_unw()
-    ch.normalise(0.24,0.34)
-    ch.normalize_unw(0.24,0.34)
-
-
-    histos = [
-            ["hSE",     ch.get_se_k()],
-            ["hME",     ch.get_me_k()],
-            ["hCF",     ch.get_correlation_function()],
-            ["hCF_unw", ch.get_cf_unw()]
-    ]
-    return histos
-
-def saveHistogrammsTH2(path, filename, config, rebin, newfile):
-    """
-    Function to save the histogramms of an "AnalysisResults.root" file to a new file.
-    Its name is the name of the infput file with the prefix "GF-output_".
-    It saves the output of the specified subwagon to an individual directory withing the file.
-    If you want the output of more substrains within the same output file, you have to call the function
-    multiple times and ster the parameter "newfile" to true only for the first time
-
-    Parameters:
-
-    path = path to the AnalysisResults.root file and the path, where the new root file will be stored
-
-    filename = filename of the AnalysisResults.root file
-
-    config = configuration name of the subwagon. Use an empty string ("") for the standard configuration.
-             use an underscore before the name of another configuration (e.g. "_ap-base")
-
-    rebin = rebinning factor to be applied ONLY to the correlation function within the correlation_handler class
-
-    newfile = bool value to create a new file or not.
-              -> for true the outputfile is called with the option "recreate"
-              -> for false the outputfile is created with the option "update"
-
-    Output:
-
-    no output.. just saving the new root file
-    """
-
-    infile = path+filename
-
-    #Get the histogramms from the input file
-    correlationHistos = getCorrelationFunctionTH2(infile, config, rebin)
-    trackHistos = getSingleParticlePlots(infile, config)
-    eventHistos = getEventHistos(infile, config)
+    if hist_type in th2:
+        ch.reweight()
 
     histos = []
-    histos.extend(correlationHistos)
-    histos.extend(trackHistos)
-    histos.extend(eventHistos)
+    for factor in rebinfactor:
+        ch.rebin(factor)
+        ch.make_cf()
+        ch.normalize(0.24, 0.34)
+        if hist_type in th2:
+            ch.make_cf_unw()
+            ch.normalize_unw(0.24, 0.34)
 
-    for n in range(len(histos)):
-        name = histos[n][0]
-        if name == "hPt":
-            nTPC = histos[n][1].Integral(0, histos[n][1].FindBin(0.75))
-        if name == "zvtxhist":
-            nEvents = histos[n][1].GetEntries()
+        h1 = [
+                ["hSE rebin: "+str(factor), ch.get_se_k().Clone()],
+                ["hME rebin: "+str(factor), ch.get_se_k().Clone()],
+                ["hCF rebin: "+str(factor), ch.get_cf().Clone()],
+        ]
+        histos.extend(h1)
+        if hist_type in th2:
+            h2 = [
+                    ["hME_unw rebin: "+str(factor), ch.get_me_k_unw().Clone()],
+                    ["hCF_unw rebin: "+str(factor), ch.get_cf_unw().Clone()],
+            ]
+            histos.extend(h2)
 
+        ch.resetbin()
 
-    #print here some interesting numbers like number of pairs in a certain k^* region, number of events, etc.
-    print(" ")
-    print("### Getting file "+filename+" with the configuration "+config+" ###")
-
-    print("Number of Events:")
-    print(histos[0][1].GetEntries())
-
-    print("nTPC/nEvents")
-    print(nTPC/nEvents)
-
-    print("Number of pairs in the low k* region")
-    print(histos[0][1].Integral(1, histos[0][1].FindBin(0.2)))
-
-
-
-    if newfile:
-        fileaction = "recreate"
-    else:
-        fileaction = "update"
-
-    outfile = path+"GF-output_"+filename
-    output = ROOT.TFile(outfile,fileaction)
-    if config=="":
-        dir = output.mkdir("_std")
-    else:
-        dir = output.mkdir(config)
-
-    dir.cd()
-
-    for i in range(len(histos)):
-        histos[i][1].Write(histos[i][0])
-
-    output.Close()
-
+    return histos
 
 def getSingleParticlePlots(infilename, config):
     """
@@ -247,8 +144,7 @@ def getEventHistos(infilename, config):
 
     return histos
 
-
-def saveHistogramms(path, filename, config, rebin, newfile):
+def saveHistogramms(path, filename, newfile, config, hist_type = 'TH1F', rebin = 2):
     """
     Function to save the histogramms of an "AnalysisResults.root" file to a new file.
     Its name is the name of the infput file with the prefix "GF-output_".
@@ -279,7 +175,7 @@ def saveHistogramms(path, filename, config, rebin, newfile):
     infile = path+filename
 
     #Get the histogramms from the input file
-    correlationHistos = getCorrelationFunction(infile, config, rebin)
+    correlationHistos = getCorrelationFunction(infile, config, hist_type, rebin)
     trackHistos = getSingleParticlePlots(infile, config)
     eventHistos = getEventHistos(infile, config)
 
@@ -315,7 +211,7 @@ def saveHistogramms(path, filename, config, rebin, newfile):
     else:
         fileaction = "update"
 
-    outfile = path+"GF-output_"+filename
+    outfile = path+"UFFA_"+filename
     output = ROOT.TFile(outfile,fileaction)
     if config=="":
         dir = output.mkdir("_std")
