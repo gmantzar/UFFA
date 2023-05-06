b3a2edbc38c7755ca5d29ef5daf5c4df7105fcf8
Jaime <jaime.gonzalez@tum.de>
2023-05-06 12:30:49 +0200

    Teample fits

--- TemplateFit.py
+++ TemplateFit.py
@@ -1,4 +1,6 @@
 import ROOT
+import FemtoDreamReader as FDR
+import FemtoAnalysis as FA
 
 class ftotal:
     def __call__(self, arr, par):
@@ -10,29 +12,24 @@ class ftotal:
         fake_adj = par[4]*prim.GetBinContent(nbin)
         return prim_adj + lam_adj + sig_adj + mat_adj + fake_adj
 
+# def TemplateFit(dirIn, fname, nfile, htype, bins, dirOut = None):
+    #conf = FA.config(dirIn, dirOut, fname, False, nfile, False, htype, False, bins, False)
+    #fdr = FDR.FemtoDreamReader(conf[0] + conf[1], conf[2])
+
 def TemplateFit(filename):
-    # TFile and TDirectory
-    ifile = ROOT.TFile(filename, "read")
-    std_dir = ifile.GetDirectory("_std")
-    mc_dir = ifile.GetDirectory("_std/MC")
-
-    # check if MC data exists in file
-    if not mc_dir:
-        print("Error: TDirectory \"MC\" does not exist!")
-        exit()
-
     # Output file
-    ofile = ROOT.TFile("TemplateFit_"+"filename", "recreate")
+    ofile = ROOT.TFile("TemplateFit_"+filename, "recreate")
+    fdr = FDR.FemtoDreamReader(filename, "")
 
     # get data
-    DCAxy_data = std_dir.Get("hDCAxy")
+    DCAxy_data = fdr.get_dca()
 
     # get MC templates
-    DCAxy_prim  = mc_dir.Get("hDCAxy_Primary")
-    DCAxy_lam   = mc_dir.Get("hDCAxy_DaughterLambda")
-    DCAxy_sig   = mc_dir.Get("hDCAxy_DaughterSigmaplus")
-    DCAxy_mat   = mc_dir.Get("hDCAxy_Material")
-    DCAxy_fake  = mc_dir.Get("hDCAxy_Fake")
+    DCAxy_prim  = fdr.get_histo("Tracks_oneMC/hDCAxy_Primary")
+    DCAxy_lam   = fdr.get_histo("Tracks_oneMC/hDCAxy_DaughterLambda")
+    DCAxy_sig   = fdr.get_histo("Tracks_oneMC/hDCAxy_DaughterSigmaplus")
+    DCAxy_mat   = fdr.get_histo("Tracks_oneMC/hDCAxy_Material")
+    DCAxy_fake  = fdr.get_histo("Tracks_oneMC/hDCAxy_Fake")
 
     # fitting function
     adj = ftotal()
@@ -42,17 +39,17 @@ def TemplateFit(filename):
     Num_pT_bins = 8
 
     # x and y lists for graphs
-    x_prim, y_prim = array('d'), array('d')
-    x_lam, y_lam = array('d'), array('d')
-    x_sig, y_sig = array('d'), array('d')
-    x_mat, y_mat = array('d'), array('d')
-    x_fake, y_fake = array('d'), array('d')
-    x_chi, y_chi = array('d'), array('d')
+    x_prim, y_prim = [], []
+    x_lam,  y_lam  = [], []
+    x_sig,  y_sig  = [], []
+    x_mat,  y_mat  = [], []
+    x_fake, y_fake = [], []
+    x_chi,  y_chi  = [], []
 
     # dca cut
     DCA_cut = 0.1
+    CPAcut = 0.1
 
-    # ???
     dataEntries = []
     mcEntries = []
     parPrim = []
@@ -65,18 +62,18 @@ def TemplateFit(filename):
         # canvas for fitting
         c = ROOT.TCanvas()
         c.cd()
-        gPad.SetLogy()
+        ROOT.gPad.SetLogy()
 
         # data
-        data = DCAxy_data.ProjectionY("DCAxy_"+n, n, n)
+        data = DCAxy_data.ProjectionY("DCAxy_" + str(n), n, n)
         data.Scale(1./data.Integral())
 
         # MC templates
-        prim   = DCAxy_prim.ProjectionY("prim_"+n, n, n)
-        lam    = DCAxy_lam.ProjectionY("lam_"+n, n, n)
-        sig    = DCAxy_sig.ProjectionY("sig_"+n, n, n)
-        mat    = DCAxy_mat.ProjectionY("mat_"+n, n, n)
-        fake   = DCAxy_fake.ProjectionY("fake_"+n, n, n)
+        prim   = DCAxy_prim.ProjectionY("prim_" + str(n), n, n)
+        lam    = DCAxy_lam.ProjectionY("lam_" + str(n), n, n)
+        sig    = DCAxy_sig.ProjectionY("sig_" + str(n), n, n)
+        mat    = DCAxy_mat.ProjectionY("mat_" + str(n), n, n)
+        fake   = DCAxy_fake.ProjectionY("fake_" + str(n), n, n)
 
         # normalize
         prim.Scale(1./prim.Integral())
@@ -95,41 +92,41 @@ def TemplateFit(filename):
 
         # draw histograms
         data.Fit("ftot", "S, N, R, M", "", 0.9, 1)
-        data.SetLineColor(kBlack)
+        data.SetLineColor(1)
         data.SetLineWidth(2)
         data.Draw("hist")
 
         prim.Scale(ftot.GetParameter(0))
-        prim.SetLineColor(kRed + 2)
+        prim.SetLineColor(2)
         prim.SetLineWidth(2)
         prim.Draw("same")
 
         lam.Scale(ftot.GetParameter(1))
-        lam.SetLineColor(kBlue + 1)
+        lam.SetLineColor(4)
         lam.SetLineWidth(2)
         lam.Draw("same")
 
         sig.Scale(ftot.GetParameter(2))
-        sig.SetLineColor(kMagenta + 3)
+        sig.SetLineColor(6)
         sig.SetLineWidth(2)
         sig.Draw("same")
 
         mat.Scale(ftot.GetParameter(3))
-        mat.SetLineColor(kGreen + 3)
+        mat.SetLineColor(3)
         mat.SetLineWidth(2)
         mat.Draw("same")
 
         fake.Scale(ftot.GetParameter(4))
-        fake.SetLineColor(kOrange + 1)
+        fake.SetLineColor(95)
         fake.SetLineWidth(2)
         fake.Draw("same")
 
-        htot = prim.Clone("htot_"+n)
+        htot = prim.Clone("htot_" + str(n))
         htot.Add(lam)
         htot.Add(sig)
         htot.Add(mat)
         htot.Add(fake)
-        htot.SetLineColor(kRed)
+        htot.SetLineColor(2)
         htot.SetLineWidth(2)
         htot.Draw("same")
 
@@ -142,13 +139,12 @@ def TemplateFit(filename):
         fake.Write()
 
         # ???
-        dataEntries.append(data.Integral(data.FindBin(DCA_cut), data.FindBin(1)))
-        mcEntries.append(htot.Integral(htot.FindBind(CPAcut), htot.FindBin(1)/mcEntries[n - 1]))
-        parPrim.append(prim.Integral(prim.FindBind(CPAcut), prim.FindBin(1)/mcEntries[n - 1]))
-        parLam.append(lam.Integral(lam.FindBind(CPAcut), lam.FindBin(1)/mcEntries[n - 1]))
-        parSig.append(sig.Integral(sig.FindBind(CPAcut), sig.FindBin(1)/mcEntries[n - 1]))
-        parMat.append(mat.Integral(mat.FindBind(CPAcut), mat.FindBin(1)/mcEntries[n - 1]))
-        parFake.append(fake.Integral(fake.FindBind(CPAcut), fake.FindBin(1)/mcEntries[n - 1]))
+        dataEntries.append(htot.Integral(htot.FindBin(DCA_cut), htot.FindBin(1)))
+        parPrim.append(prim.Integral(prim.FindBin(DCA_cut), prim.FindBin(1)) / dataEntries[n - 1])
+        parLam.append(lam.Integral(lam.FindBin(DCA_cut), lam.FindBin(1)) / dataEntries[n - 1])
+        parSig.append(sig.Integral(sig.FindBin(DCA_cut), sig.FindBin(1)) / dataEntries[n - 1])
+        parMat.append(mat.Integral(mat.FindBin(DCA_cut), mat.FindBin(1)) / dataEntries[n - 1])
+        parFake.append(fake.Integral(fake.FindBin(DCA_cut), fake.FindBin(1)) / dataEntries[n - 1])
 
         # fill x and y lists for graphs
         x_prim.append(DCAxy_data.GetXaxis().GetBinCenter(n))
@@ -162,7 +158,7 @@ def TemplateFit(filename):
         y_sig.append(parSig[n - 1])
         y_mat.append(parMat[n - 1])
         y_fake.append(parFake[n - 1])
-        y_chi.append(ftot.GetChisquare() / ftot.GetNDF()))
+        y_chi.append(ftot.GetChisquare() / ftot.GetNDF())
 
     gPrim = TGraph(Num_pT_bin - 1, x_prim, y_prim)
     gLam = TGraph(Num_pT_bin - 1, x_lam, y_lam)
@@ -171,12 +167,12 @@ def TemplateFit(filename):
     gFake = TGraph(Num_pT_bin - 1, x_fake, y_fake)
     gChi = TGraph(Num_pT_bin - 1, x_chi, y_chi)
 
-	gPrim->SetName("Primary");
-	gLam->SetName("Lambda");
-	gSig->SetName("Sigma+");
-	gMat->SetName("Material");
-	gFake->SetName("Fake");
-	gChi->SetName("Chi");
+    gPrim.SetName("Primary")
+    gLam.SetName("Lambda")
+    gSig.SetName("Sigma+")
+    gMat.SetName("Material")
+    gFake.SetName("Fake")
+    gChi.SetName("Chi")
 
     # im not sure whats happening here but i might know when im finished
     c2 = ROOT.TCanvas("c2", "c2", 0, 0, 650, 550)
@@ -203,41 +199,41 @@ def TemplateFit(filename):
 
     # what even is this
     d12 = ROOT.TCanvas("d12", "d12", 0, 0, 650, 550)
-    d12->cd()
-    gPrim->SetLineWidth(2)
-    gLam->SetLineWidth(2)
-    gSig->SetLineWidth(2)
-    gMat->SetLineWidth(2)
-    gFake->SetLineWidth(2)
-    gChi->SetLineWidth(2)
-    gPrim->GetXaxis()->SetLabelSize(0.05)
-    gPrim->GetYaxis()->SetLabelSize(0.05)
-    gPrim->GetXaxis()->SetTitleSize(0.05)
-    gPrim->GetXaxis()->SetTitle("p (GeV)")
-    gChi->GetXaxis()->SetLabelSize(0.05)
-    gChi->GetYaxis()->SetLabelSize(0.05)
-    gChi->GetYaxis()->SetTitle("chi2/ndf")
-    gChi->GetXaxis()->SetTitle("p (GeV)")
-    gChi->GetXaxis()->SetTitleSize(0.05)
-    gChi->GetYaxis()->SetTitleSize(0.05)
+    d12.cd()
+    gPrim.SetLineWidth(2)
+    gLam.SetLineWidth(2)
+    gSig.SetLineWidth(2)
+    gMat.SetLineWidth(2)
+    gFake.SetLineWidth(2)
+    gChi.SetLineWidth(2)
+    gPrim.GetXaxis().SetLabelSize(0.05)
+    gPrim.GetYaxis().SetLabelSize(0.05)
+    gPrim.GetXaxis().SetTitleSize(0.05)
+    gPrim.GetXaxis().SetTitle("p (GeV)")
+    gChi.GetXaxis().SetLabelSize(0.05)
+    gChi.GetYaxis().SetLabelSize(0.05)
+    gChi.GetYaxis().SetTitle("chi2/ndf")
+    gChi.GetXaxis().SetTitle("p (GeV)")
+    gChi.GetXaxis().SetTitleSize(0.05)
+    gChi.GetYaxis().SetTitleSize(0.05)
 
-    gPrim->SetLineColor(kRed + 2)
-    gPrim->GetYaxis()->SetRangeUser(0.,1.)
-    gPrim->Draw("")
-    gLam->SetLineColor(kBlue + 1)
-    gLam->Draw("same")
-    gSig->SetLineColor(kMagenta + 3)
-    gSig->Draw("same")
-    gMat->SetLineColor(kGreen + 3)
-    gMat->Draw("same")
-    gFake->SetLineColor(kOrange + 1)
-    gFake->Draw("same")
+    gPrim.SetLineColor(kRed + 2)
+    gPrim.GetYaxis().SetRangeUser(0.,1.)
+    gPrim.Draw("")
+    gLam.SetLineColor(kBlue + 1)
+    gLam.Draw("same")
+    gSig.SetLineColor(kMagenta + 3)
+    gSig.Draw("same")
+    gMat.SetLineColor(kGreen + 3)
+    gMat.Draw("same")
+    gFake.SetLineColor(kOrange + 1)
+    gFake.Draw("same")
 
     primAvg = TF1("primAvg", "[0]", 0, 5)
-    primAvg->SetParameter(0, m2prim / m2tot)
-    primAvg->SetLineColor(kBlack)
-    primAvg->SetLineStyle(2)
-    primAvg->Draw("same")
+    primAvg.SetParameter(0, m2prim / m2tot)
+    primAvg.SetLineColor(kBlack)
+    primAvg.SetLineStyle(2)
+    primAvg.Draw("same")
 
     ofile.cd()
     gPrim.Write()
