c5f4c9e8269b2de932eb2ac64a1d6f314d714ba9
Jaime <jaime.gonzalez@tum.de>
2023-05-11 16:19:34 +0200

    I MISSPELLED

--- FemtoDreamReader.py
+++ FemtoDreamReader.py
@@ -5,8 +5,8 @@ class FemtoDreamReader(FR.FileReader):
     def __init__(self, ifile, directory = None):
         if directory == "":
             directory = "femto-dream-pair-task-track-track"
-        elif directory[0] == '_':
-            directory =="femto-dream-pair-task-track-track" + directory
+        if directory[0] == '_':
+            directory = "femto-dream-pair-task-track-track" + directory
         FR.FileReader.__init__(self, ifile, directory)
 
     ### Getter Functions ###
